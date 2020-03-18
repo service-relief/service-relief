260fc50d6c03f5b4967b3eb5babbe5d3151f38a2
Dustin Schau <dustinschau@gmail.com>
2020-03-18 10:53:15 -0700

    chore: use theme in starter

--- examples/starter/gatsby-config.js
+++ examples/starter/gatsby-config.js
@@ -1,11 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Service Relief`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-theme-service-relief`,
+      options: {
+
+      }
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
