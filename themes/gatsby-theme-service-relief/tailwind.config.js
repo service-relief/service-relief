42d4efe7183e62cbc09afe6a7cb14091d38c9f5e
Amberley Romo <amberleyjohanna@gmail.com>
2020-03-19 13:26:28 -0500

    wip

--- themes/gatsby-theme-service-relief/tailwind.config.js
+++ themes/gatsby-theme-service-relief/tailwind.config.js
@@ -4,13 +4,13 @@ module.exports = {
       sm: "640px",
       md: "768px",
       lg: "1024px",
-      xl: "1280px",
+      xl: "1280px"
     },
     fontFamily: {
       heading: ["Bungee", "sans-serif"]
     },
-    extend: {},
+    extend: {}
   },
   variants: {},
-  plugins: [],
-}
+  plugins: []
+};
