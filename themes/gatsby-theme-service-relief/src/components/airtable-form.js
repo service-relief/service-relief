232df219e5bfef7042f1e8db5b0e8f6a0cb0efff
Dustin Schau <dustinschau@gmail.com>
2020-03-19 20:14:15 -0700

    chore: forward props

--- themes/gatsby-theme-service-relief/src/components/airtable-form.js
+++ themes/gatsby-theme-service-relief/src/components/airtable-form.js
@@ -4,7 +4,7 @@ import ScriptLoader from './script-loader'
 
 function AirtableForm({ id, height = 1237, ...rest }) {
   return (
-    <ScriptLoader src={`https://static.airtable.com/js/embed/embed_snippet_v1.js`}>
+    <ScriptLoader src={`https://static.airtable.com/js/embed/embed_snippet_v1.js`} {...rest}>
       <iframe className="airtable-embed airtable-dynamic-height" src={`https://airtable.com/embed/${id}?backgroundColor=gray`} frameBorder="0" width="100%" height={height} style={{
       background: `transparent`,
       border: `1px solid #ccc`
